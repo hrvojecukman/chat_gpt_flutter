13ab21b15fe92ba69bd8bc4eb7e424ec424a4d9a
Hrvoje Čukman <hrvoje.cukman@gmail.com>
2023-02-09 14:30:14 +0100

    Added generated code

--- lib/src/models/completion_response.g.dart
+++ lib/src/models/completion_response.g.dart
@@ -8,16 +8,17 @@ part of 'completion_response.dart';
 
 CompletionResponse _$CompletionResponseFromJson(Map<String, dynamic> json) =>
     CompletionResponse(
-      id: json['id'] as String,
-      object: json['object'] as String,
-      created: json['created'] as int,
-      model: json['model'] as String,
-      choices: (json['choices'] as List<dynamic>)
-          .map((e) => ChoicesResponse.fromJson(e as Map<String, dynamic>))
+      id: json['id'] as String?,
+      object: json['object'] as String?,
+      created: json['created'] as int?,
+      model: json['model'] as String?,
+      choices: (json['choices'] as List<dynamic>?)
+          ?.map((e) => ChoicesResponse.fromJson(e as Map<String, dynamic>))
           .toList(),
       usage: json['usage'] == null
           ? null
           : UsageResponse.fromJson(json['usage'] as Map<String, dynamic>),
+      streamMessageEnd: json['stream_message_end'] as bool? ?? false,
     );
 
 Map<String, dynamic> _$CompletionResponseToJson(CompletionResponse instance) =>
@@ -28,4 +29,5 @@ Map<String, dynamic> _$CompletionResponseToJson(CompletionResponse instance) =>
       'model': instance.model,
       'choices': instance.choices,
       'usage': instance.usage,
+      'stream_message_end': instance.streamMessageEnd,
     };

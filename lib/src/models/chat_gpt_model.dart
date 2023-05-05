43b98a77e1c2ba0cd1b59b6aae978db2cbd6e2a5
Matej Kovacevic 2 <matej.kovacevic2@q.agency>
2023-05-05 11:13:06 +0200

    added new property to enum chatgpt model

--- lib/src/models/chat_gpt_model.dart
+++ lib/src/models/chat_gpt_model.dart
@@ -30,11 +30,17 @@ enum ChatGptModel {
   @JsonValue('text-curie-001')
   textCurie001,
   @JsonValue('gpt-3.5-turbo')
-  gpt35Turbo,
+  gpt35Turbo(modelName: 'gpt-3.5-turbo'),
   @JsonValue('gpt-3.5-turbo-0301')
   gpt35Turbo0301,
   @JsonValue('gpt-4')
-  gpt4,
+  gpt4(modelName: 'gpt-4'),
   @JsonValue('gpt-4-0314')
   gpt40314;
+
+  final String modelName;
+
+  const ChatGptModel({
+    this.modelName = '',
+  });
 }
